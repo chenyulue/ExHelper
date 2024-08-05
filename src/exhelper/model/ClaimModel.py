5ae93c5183e41e975576c390852e9aab7cf5d228
chenyulue <chenyulue@163.com>
2024-08-05 21:20:34 +0800

    claim parse

--- src/exhelper/model/ClaimModel.py
+++ src/exhelper/model/ClaimModel.py
@@ -1,6 +1,14 @@
-from typing import Iterator
+from typing import Iterator, TypedDict
+import re
+from collections import defaultdict
 
 from . import SearchModel, ConfigModel, SearchResult
+
+class Claim(TypedDict):
+    content: str
+    subject: str
+    position: tuple[int, int]
+    
 
 class ClaimModel:
     def __init__(self, claims: str, setting: ConfigModel) -> None:
@@ -17,5 +25,13 @@ class ClaimModel:
         self.search.set_search_pattern("|".join(self._unclear_words))
         return self.search.search()
 
-    def _parse_claims(self) -> dict[int, Any]:
-        
+    def _parse_claims(self) -> None:
+        pattern = r"^(\d+)\s*[.、]\s*([^，]+)，(?:.|\n(?!^\d|$))+"
+        self.search.set_search_pattern(pattern, re.MULTILINE)
+        self.search.reset_search_model(content=self._claims)
+        self.claims = defaultdict(dict)
+        for match, start, end in self.search.search():
+            claim_num = int(match.group(1))
+            self.claims[claim_num]["content"] = match.group(0)
+            self.claims[claim_num]["subject"] = match.group(2)
+            self.claims[claim_num]["position"] = (start, end)
